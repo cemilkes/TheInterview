4dd505dedab7ba7f74d08b352b10d4b5fff60b90
CemilKes <kes.cemil@gmail.com>
2021-06-16 08:50:22 -0700

    136 Single Number

--- TheInterview/136_Single_Number.h
+++ TheInterview/136_Single_Number.h
@@ -8,9 +8,13 @@
 #ifndef _136_Single_Number_h
 #define _136_Single_Number_h
 int singleNumber(vector<int>& nums) {
-        
+    int singleNumber = 0;
     
-    return 0;
+    for (int i = 0; i < nums.size(); i++) {
+        singleNumber ^= nums[i];
+    }
+    
+    return singleNumber;
 }
 
 #endif /* _136_Single_Number_h */
