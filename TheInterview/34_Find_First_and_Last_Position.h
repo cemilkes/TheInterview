e8129319fe26a8b451fbe79741e0218295ececc7
CemilKes <kes.cemil@gmail.com>
2021-06-22 10:26:48 -0700

    705 Design HashSet

---
+++ TheInterview/34_Find_First_and_Last_Position.h
@@ -0,0 +1,12 @@
+//
+//  34_Find_First_and_Last_Position.h
+//  TheInterview
+//
+//  Created by Mr Kes on 6/21/21.
+//
+
+#ifndef _4_Find_First_and_Last_Position_h
+#define _4_Find_First_and_Last_Position_h
+
+
+#endif /* _4_Find_First_and_Last_Position_h */
---
+++ TheInterview/705_Design_Hashset.h
@@ -0,0 +1,12 @@
+//
+//  705_Design_Hashset.h
+//  TheInterview
+//
+//  Created by Mr Kes on 6/22/21.
+//
+
+#ifndef _05_Design_Hashset_h
+#define _05_Design_Hashset_h
+
+
+#endif /* _05_Design_Hashset_h */
