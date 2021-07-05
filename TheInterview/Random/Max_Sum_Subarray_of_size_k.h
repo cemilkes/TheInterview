81a6cb3e0eed8c6666f6458941c8c40f8a627c2a
CemilKes <kes.cemil@gmail.com>
2021-07-05 16:03:32 -0700

    Commit changes

--- TheInterview/Random/Max_Sum_Subarray_of_size_k.h
+++ TheInterview/Random/Max_Sum_Subarray_of_size_k.h
@@ -8,6 +8,13 @@
 #ifndef Max_Sum_Subarray_of_size_k_h
 #define Max_Sum_Subarray_of_size_k_h
 
+/*
+ consider each subarray as a Sliding Window of size ‘k.’ To calculate the sum of the next subarray, we need to slide the window ahead by one element. So to slide the window forward and calculate the sum of the new position of the sliding window, we need to do two things:
+
+ Subtract the element going out of the sliding window, i.e., subtract the first element of the window.
+ Add the new element getting included in the sliding window, i.e., the element coming right after the end of the window.
+ */
+
 static int findMaxSumSubArray(int k, const vector<int>& arr) {
     int maxSum = 0;
     int windowSum = 0;
@@ -28,6 +35,8 @@ static int findMaxSumSubArray(int k, const vector<int>& arr) {
 
 #endif /* Max_Sum_Subarray_of_size_k_h */
 
+
+//A basic brute force solution will be to calculate the sum of all ‘k’ sized subarrays of the given array to find the subarray with the highest sum. We can start from every index of the given array and add the next ‘k’ elements to find the subarray’s sum.
 // MARK: - Brute Force
 /*
  static int findMaxSumSubArray(int k, const vector<int>& arr) {
